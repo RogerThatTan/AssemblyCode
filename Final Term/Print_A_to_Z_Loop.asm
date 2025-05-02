6e821a3c94f955e296064de5efc55d8bcf45fb72
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-05-02 15:02:38 +0600

    final-init

---
+++ Final Term/Print_A_to_Z_Loop.asm
@@ -0,0 +1,25 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+
+
+.CODE
+MAIN PROC
+    
+    MOV CX,26
+    MOV AH,2
+    MOV DL,'A'
+    
+    TOP:
+    INT 21H
+    INC DL
+    
+    LOOP TOP:
+
+    
+    EXIT:
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
+  
---
+++ Final Term/Using_NEG_OP_CODE.asm
@@ -0,0 +1,18 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+ 
+.CODE
+MAIN PROC
+    
+    
+    MOV CL,5
+    NEG CL
+    
+
+    
+    EXIT:
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
