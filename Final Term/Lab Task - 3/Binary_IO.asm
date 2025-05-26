14db8fd5c0e1f5f383cd7feee85bd807c5848f05
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-05-26 11:26:09 +0600

    bianry

---
+++ Final Term/Lab Task - 3/Binary_IO.asm
@@ -0,0 +1,58 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    MSG1 DB "INPUT: $"
+    MSG2 DB 10,13,"OUTPUT: $"
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    MOV AH,9
+    LEA DX,MSG1
+    INT 21H
+    
+    MOV BX,0
+    MOV CX,16
+    
+    INPUT_P:
+    
+            MOV AH,1
+            INT 21H
+            CMP AL,13               ;ENTER PRESSED CHECK
+            JE  OUTPUT
+            AND AL,0FH
+            SHL BX,1
+            OR BL,AL
+            LOOP INPUT_p
+    OUTPUT:
+            MOV AH,9
+            LEA DX,MSG2
+            INT 21H
+            
+            MOV CX,16  
+            
+    OUTPUT_DIGIT:
+            
+            SHL BX,1
+            JNC ZERO
+            MOV DL,49
+            MOV AH,2
+            INT 21H
+            JMP OUTPUT_2
+    ZERO:
+            
+            MOV DL,48
+            MOV AH,2
+            INT 21H
+            
+    OUTPUT_2:
+            
+            LOOP OUTPUT_DIGIT
+
+    EXIT: 
+            
+            MOV AH,4CH
+            INT 21H
+            MAIN ENDP
+END MAIN
