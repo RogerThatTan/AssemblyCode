9c03a1936fe54c7ed31ab185687f629142070c4d
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-05-28 18:31:28 +0600

    hex

--- Final Term/Lab Task - 3/Hex_IO.asm
+++ Final Term/Lab Task - 3/Hex_IO.asm
@@ -13,37 +13,37 @@ MAIN PROC
     INT 21H
     
     MOV BX,0
-    MOV CL,4
+    MOV CX,4
     
     INPUT:
     MOV AH,1
     INT 21H
-    CMP AL,0DH 
+    CMP AL,13 
     JE OUTPUT
     CMP AL,41H ;41H =A
     JGE LETTER
     AND AL,0FH ;sub al, 30h  ; if input = 6 then 36h -30h = 6h
     JMP SHIFT
+   
     
     LETTER:
     SUB AL,37H ; 41h - 37h = 10(A)
     
     SHIFT:
-    SHL BX,CL
+    SHL BX,4
     OR BL,AL
-    JMP INPUT
+    LOOP INPUT
     
     OUTPUT:
-    MOV AH,2
-    MOV DL,0DH
-    INT 21H
-    MOV DL,0AH
+    MOV AH,9
+    LEA DX,MSG2
     INT 21H
     
     MOV CX,4
-    MOV AH,2
+    
     
     FOR2:
+    MOV AH,2
     MOV DL,BH
     SHR DL,4
     ROL BX,4
@@ -52,18 +52,16 @@ MAIN PROC
     
     ADD DL,30H
     INT 21H
-    JMP EXIT2
+    JMP EXIT
     
     OUTPUT_LETTER:
     ADD DL,37H
-    INT 21H
+    INT 21H 
     
-    EXIT2:
+    
+    EXIT:
     LOOP FOR2
     MOV  AH,4CH
     INT 21H
     MAIN ENDP
 END MAIN
-        
-    
-    
