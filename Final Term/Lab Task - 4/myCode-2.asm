e79c90152ef1636a7044f83e4975e4b346820c92
Tanvir Hassan <22-46481-1@student.aiub.edu>
2025-06-01 14:09:48 +0600

    lab-4

--- Final Term/Lab Task - 4/myCode-2.asm
+++ Final Term/Lab Task - 4/myCode-2.asm
@@ -1,13 +1,51 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+ 
+  MSG1 DB "INPUT: $"
+  MSG2 DB 10,13,"OUTPUT: $"
 
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
+.CODE
+MAIN PROC
+    
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    MOV AH,9
+    LEA DX,MSG1
+    INT 21H
+    
+    MOV CX,8
+    MOV BL,0
+INPUT_LOOP:
+    MOV AH,1
+    INT 21H
+    SUB AL,'0'
+    SHL BL, 1           
+    OR BL, AL           
+    LOOP INPUT_LOOP
+    
+    NOT BL 
+    
+    MOV AH, 9
+    LEA DX, MSG2
+    INT 21H
 
-org 100h
-
-; add your code here
-
-ret
-
-
-
-
+    MOV CX, 8
+OUTPUT_LOOP:
+    SHL BL, 1           
+    JC  ONE      
+    MOV DL, '0'         
+    JMP DISPLAY
+ONE:
+    MOV DL, '1'         
+DISPLAY:
+    MOV AH, 2
+    INT 21H
+    LOOP OUTPUT_LOOP
+    
+    EXIT:
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
